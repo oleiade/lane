be6181a34b6c850c0d7e05c1793524af3516f591
Oleiade <tcrevon@gmail.com>
2013-08-29 23:37:18 +0200

    Updated stack to be based on Deque data structure

--- stack.go
+++ stack.go
@@ -1,67 +1,23 @@
 package lane
 
-import (
-	"container/list"
-	"sync"
-)
-
 type Stack struct {
-	sync.RWMutex
-	container *list.List
+	*Deque
 }
 
 func NewStack() *Stack {
 	return &Stack{
-		container: list.New(),
+		Deque: NewDeque(),
 	}
 }
 
 func (s *Stack) Push(item interface{}) {
-	s.Lock()
-	defer s.Unlock()
-
-	s.container.PushFront(item)
-
-	return
+	s.Prepend(item)
 }
 
 func (s *Stack) Pop() interface{} {
-	s.Lock()
-	defer s.Unlock()
-
-	var item interface{} = nil
-	var firstListItem *list.Element = nil
-
-	firstListItem = s.container.Front()
-	if firstListItem != nil {
-		item = s.container.Remove(firstListItem)
-	}
-
-	return item
-}
-
-func (s *Stack) Size() int {
-	s.RLock()
-	defer s.RUnlock()
-
-	return s.container.Len()
-}
-
-func (s *Stack) Empty() bool {
-	s.RLock()
-	defer s.RUnlock()
-
-	return s.container.Len() == 0
+	return s.Shift()
 }
 
 func (s *Stack) Head() interface{} {
-	s.RLock()
-	defer s.RUnlock()
-
-	item := s.container.Front()
-	if item != nil {
-		return item.Value
-	} else {
-		return nil
-	}
+	return s.First()
 }
