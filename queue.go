ac86b8e5f0a5847d62485cba82e865e703385ae7
Oleiade <tcrevon@gmail.com>
2013-08-29 23:49:31 +0200

    Added documentation to queue code

--- queue.go
+++ queue.go
@@ -1,5 +1,13 @@
 package lane
 
+// Queue is a FIFO (First in first out) data structure implementation.
+// It is based on a deque container and focuses its API on core
+// functionalities: Enqueue, Dequeue, Head, Size, Empty. Every operations time complexity
+// is O(1).
+//
+// As it is implemented using a Deque container, every operations
+// over an instiated Stack are synchronized and safe for concurrent
+// usage.
 type Queue struct {
 	*Deque
 }
@@ -10,14 +18,17 @@ func NewQueue() *Queue {
 	}
 }
 
+// Enqueue adds an item at the back of the queue
 func (q *Queue) Enqueue(item interface{}) {
 	q.Prepend(item)
 }
 
+// Dequeue removes and returns the front queue item
 func (q *Queue) Dequeue() interface{} {
 	return q.Pop()
 }
 
+// Head returns the front queue item
 func (q *Queue) Head() interface{} {
 	return q.Last()
 }
