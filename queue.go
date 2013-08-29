985c2825e4f25a718b5a04701f81320d78217c7c
Oleiade <tcrevon@gmail.com>
2013-08-29 14:44:11 +0200

    Bootstraped queue data structure

---
+++ queue.go
@@ -0,0 +1,19 @@
+package lane
+
+type Queue []interface{}
+
+func NewQueue(items []interface) *Queue {
+	
+}
+
+func (q *Queue) Enqueue(item interface{}) {
+	
+}
+
+func (q *Queue) Dequeue() interface{} {
+	
+}
+
+func (q *Queue) Size() interface{} {
+	
+}
