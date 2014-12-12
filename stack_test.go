36813299dfd665f50ac259c100a417f9cae21918
oleiade <tcrevon@gmail.com>
2014-12-12 16:07:23 +0100

    Improve stack testing

--- stack_test.go
+++ stack_test.go
@@ -1,46 +1,83 @@
 package lane
 
 import (
-	"github.com/stretchr/testify/assert"
+	"strconv"
 	"testing"
 )
 
 func TestStackPush(t *testing.T) {
 	stack := NewStack()
+	stackSize := 100
 
-	stack.Push("1")
-	stack.Push("2")
-	stack.Push("3")
+	// Fulfill the test Stack
+	for i := 0; i < stackSize; i++ {
+		var value string = strconv.Itoa(i)
+		stack.Push(value)
+	}
 
-	assert.True(t, stack.container.Len() == 3)
-	assert.Equal(t, stack.container.Front().Value, "3")
-	assert.Equal(t, stack.container.Back().Value, "1")
+	assert(
+		t,
+		stack.container.Len() == stackSize,
+		"stack.container.Len() = %d; want %d", stack.container.Len(), stackSize,
+	)
+
+	assert(
+		t,
+		stack.container.Front().Value == "99",
+		"stack.container.Front().Value = %s; want %s", stack.container.Front().Value, "99",
+	)
+
+	assert(
+		t,
+		stack.container.Back().Value == "0",
+		"stack.container.Back().Value = %s; want %s", stack.container.Back().Value, "0",
+	)
 }
 
 func TestStackPop_fulfilled(t *testing.T) {
 	stack := NewStack()
+	stackSize := 100
 
-	stack.Push("1")
-	stack.Push("2")
-	stack.Push("3")
+	// Add elements to the test Stack
+	for i := 0; i < stackSize; i++ {
+		var value string = strconv.Itoa(i)
+		stack.Push(value)
+	}
 
-	item := stack.Pop()
-	assert.Equal(t, item, "3")
-	assert.Equal(t, stack.container.Len(), 2)
-	item = stack.Pop()
-	assert.Equal(t, item, "2")
-	assert.Equal(t, stack.container.Len(), 1)
-	item = stack.Pop()
-	assert.Equal(t, item, "1")
-	assert.Equal(t, stack.container.Len(), 0)
+	// Pop elements and assert they come out in LIFO order
+	for i := 99; i >= 0; i-- {
+		var expectedValue string = strconv.Itoa(i)
+		item := stack.Pop()
+
+		assert(
+			t,
+			item == expectedValue,
+			"stack.Pop() = %v; want %v", item, expectedValue,
+		)
+
+		assert(
+			t,
+			stack.container.Len() == i,
+			"stack.container.Len() = %d; want %d", stack.container.Len(), i,
+		)
+	}
 }
 
 func TestStackPop_empty(t *testing.T) {
 	stack := NewStack()
 
 	item := stack.Pop()
-	assert.Equal(t, item, nil)
-	assert.Equal(t, stack.container.Len(), 0)
+	assert(
+		t,
+		item == nil,
+		"stack.Pop() = %v; want %v", item, nil,
+	)
+
+	assert(
+		t,
+		stack.container.Len() == 0,
+		"stack.container.Len() = %d; want %d", stack.container.Len(), 0,
+	)
 }
 
 func TestStackHead_fulfilled(t *testing.T) {
@@ -51,29 +88,51 @@ func TestStackHead_fulfilled(t *testing.T) {
 	stack.Push("3")
 
 	item := stack.Head()
-	assert.Equal(t, item, "3")
-	assert.Equal(t, stack.container.Len(), 3)
+	assert(
+		t,
+		item == "3",
+		"stack.Head() = %v; want %v", item, "3",
+	)
+
+	assert(
+		t,
+		stack.container.Len() == 3,
+		"stack.container.Len() = %d; want %d", stack.container.Len(), 3,
+	)
 }
 
 func TestStackHead_empty(t *testing.T) {
 	stack := NewStack()
 
 	item := stack.Head()
-	assert.Equal(t, item, nil)
-	assert.Equal(t, stack.container.Len(), 0)
+	assert(
+		t,
+		item == nil,
+		"stack.Head() = %v; want %v", item, nil,
+	)
+
+	assert(
+		t,
+		stack.container.Len() == 0,
+		"stack.container.Len() = %d; want %d", stack.container.Len(), 0,
+	)
 }
 
 func TestStackEmpty_fulfilled(t *testing.T) {
 	stack := NewStack()
-
 	stack.Push("1")
-	stack.Push("2")
-	stack.Push("3")
-
-	assert.False(t, stack.Empty())
+	assert(
+		t,
+		stack.Empty() == false,
+		"stack.Empty() = %b; want %b", stack.Empty(), false,
+	)
 }
 
 func TestStackEmpty_empty_queue(t *testing.T) {
 	stack := NewStack()
-	assert.True(t, stack.Empty())
+	assert(
+		t,
+		stack.Empty() == true,
+		"stack.Empty() = %b; want %b", stack.Empty(), true,
+	)
 }
